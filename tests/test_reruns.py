2b703f5815b333224aa77930622a2d4b2f277d4d
Есмёнов Семён Анатольевич <s.a.esmenov@corp.lis-dspr.ru>
2025-04-27 20:15:37 +0300

    6.8 Перезапуски автотестов в Pytest

---
+++ tests/test_reruns.py
@@ -0,0 +1,16 @@
+import pytest
+import random
+
+@pytest.mark.flaky(reruns=3, reruns_delay=2)
+def test_reruns():
+    assert  random.choice([True, False])
+
+
+
+PLATFORM = "Windows"
+
+@pytest.mark.flaky(reruns=3, reruns_delay=2, condition=PLATFORM == 'Windows')
+def test_reruns_with_condition():
+    assert  random.choice([True, False])
+
+
